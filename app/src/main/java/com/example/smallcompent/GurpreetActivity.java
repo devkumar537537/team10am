8f57114dac70ccdbd67023ceadd22bfd7f524d45
gurpreet <preetdhiman1331@gmail.com>
2022-01-01 10:25:45 +0530

    my first message

---
+++ app/src/main/java/com/example/smallcompent/GurpreetActivity.java
@@ -0,0 +1,14 @@
+package com.example.smallcompent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GurpreetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gurpreet);
+    }
+}
